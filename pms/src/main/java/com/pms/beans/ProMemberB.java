14e5a18bfc3df044a4375aa31a9990848102e148
jisoochoii <85734049+jisoochoii@users.noreply.github.com>
2022-07-27 21:43:34 +0900

    Add files via upload

---
+++ pms/src/main/java/com/pms/beans/ProMemberB.java
@@ -0,0 +1,12 @@
+package com.pms.beans;
+
+import lombok.Data;
+
+@Data
+public class ProMemberB {
+		private String pmbCode;
+		private String spmbCode;
+		private String proPosition;
+		private String proAccept;
+		private String proEmail;
+}
